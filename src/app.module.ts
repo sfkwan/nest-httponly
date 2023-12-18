dd02441742a2277ad3fcb9f3de46491c0cde8f99
Preston Kwan <sfkwan@yahoo.com>
2023-12-19 07:22:59 +0800

    enable middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,18 @@
-import { Logger, Module } from '@nestjs/common';
+import { Logger, MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ApiconfigModule } from './apiconfig/apiconfig.module';
 import { CatsModule } from './cats/cats.module';
 import { LoginModule } from './login/login.module';
+import { SimpleLoggerMiddleware } from './middleware/simple-logger.middleware';
 
 @Module({
   imports: [ApiconfigModule, CatsModule, LoginModule],
   controllers: [AppController],
   providers: [AppService, Logger],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(SimpleLoggerMiddleware).forRoutes('*');
+  }
+}
--- src/middleware/simple-logger.middleware.ts
+++ src/middleware/simple-logger.middleware.ts
@@ -1,10 +1,24 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Injectable, NestMiddleware, Logger } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
 
 @Injectable()
 export class SimpleLoggerMiddleware implements NestMiddleware {
-  use(req: any, res: any, next: () => void) {
-    console.log(`Executing request ...`);
+  private logger = new Logger('SimpleLoggerMiddleware');
 
+  use(req: Request, res: Response, next: NextFunction) {
+    const { ip, method, baseUrl, body } = req;
+    const userAgent = req.get('user-agent') || '';
+
+    const resBody = JSON.stringify(body);
+
+    res.on('finish', () => {
+      const { statusCode } = res;
+      const contentLength = res.get('Content-Length');
+
+      this.logger.log(
+        `${method} ${baseUrl} ${statusCode} ${contentLength} - ${userAgent} ${ip} ${resBody}`,
+      );
+    });
     next();
   }
 }
